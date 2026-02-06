64016cbfda5c40af025e192102cc97f0080e4512
Francisco Rodriguez <franciscojrdzrdz@gmail.com>
2026-02-05 23:07:02 -0800

    first commit

---
+++ packages/thor/test/thor_test.dart
@@ -0,0 +1,16 @@
+import 'package:thor/thor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
